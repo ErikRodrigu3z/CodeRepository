6b7a370d8a4b5b4434d51462b95efc9cf4c3d29c
erik.rodrigu3z@hotmail.com <erik.rodrigu3z@hotmail.com>
2021-11-10 02:43:17 -0700

    x

--- CodeRepository/AppForms/Main.cs
+++ CodeRepository/AppForms/Main.cs
@@ -1,14 +1,4 @@
-﻿using CodeRepository.Models;
-using CodeRepository.Repository.CategoryRepo;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using CodeRepository.Repository.CategoryRepo;
 
 namespace CodeRepository.AppForms
 {
